c2dc75800ead65dc22638255ae7c09f429e13cba
Phelim Bradley <wave@phel.im>
2016-09-10 16:03:02 +0100

    Use k for key not c

--- rhd/rhd.py
+++ rhd/rhd.py
@@ -11,34 +11,34 @@ class RedisCluster(object):
         self.num_conns = len(self.connections)
 
     @pipeiflist
-    def set(self, c, _bytes, conn=None):
-        conn.set(c, _bytes)
+    def set(self, k, _bytes, conn=None):
+        conn.set(k, _bytes)
 
     @pipeiflist
-    def setbit(self, c, i, bit, conn=None):
-        conn.setbit(c, i, bit)
+    def setbit(self, k, i, bit, conn=None):
+        conn.setbit(k, i, bit)
 
     @pipeiflist
-    def get(self, c, conn=None):
-        return conn.get(c)
+    def get(self, k, conn=None):
+        return conn.get(k)
 
     @pipeiflist
-    def getbit(self, c, i, conn=None):
-        return conn.getbit(c, i)
+    def getbit(self, k, i, conn=None):
+        return conn.getbit(k, i)
 
-    def get_connection(self, c):
-        if isinstance(c, str):
-            c = str.encode(c)
-        elif isinstance(c, int):
-            c = str.encode(str(c))
-        return self._get_connection_from_crc16(crc16.crc16xmodem(c))
+    def get_connection(self, k):
+        if isinstance(k, str):
+            k = str.encode(k)
+        elif isinstance(k, int):
+            k = str.encode(str(k))
+        return self._get_connection_from_crc16(crc16.crc16xmodem(k))
 
-    def get_connection_index(self, c):
-        if isinstance(c, str):
-            c = str.encode(c)
-        elif isinstance(c, int):
-            c = str.encode(str(c))
-        return self._get_connection_index_from_crc16(crc16.crc16xmodem(c))
+    def get_connection_index(self, k):
+        if isinstance(k, str):
+            k = str.encode(k)
+        elif isinstance(k, int):
+            k = str.encode(str(k))
+        return self._get_connection_index_from_crc16(crc16.crc16xmodem(k))
 
     def _get_connection_index_from_crc16(self, crc16):
         return crc16 % self.num_conns
@@ -56,24 +56,24 @@ class Rhd(RedisCluster):
     def __init(self, connections):
         super().__init__(connections)
 
-    def _index_to_mask_index(self, c):
-        if isinstance(c, list):
-            return ["".join([str(i), "_mask"]) for i in c]
+    def _index_to_mask_index(self, k):
+        if isinstance(k, list):
+            return ["".join([str(i), "_mask"]) for i in k]
         else:
-            return "".join([str(c), "_mask"])
+            return "".join([str(k), "_mask"])
 
-    def set_mask(self, c, _bytes):
-        c = self._index_to_mask_index(c)
-        self.set(c, _bytes)
+    def set_mask(self, k, _bytes):
+        k = self._index_to_mask_index(k)
+        self.set(k, _bytes)
 
-    def setbit_mask(self, c, i, bit):
-        c = self._index_to_mask_index(c)
-        self.setbit(c, i, bit)
+    def setbit_mask(self, k, i, bit):
+        k = self._index_to_mask_index(k)
+        self.setbit(k, i, bit)
 
-    def get_mask(self, c):
-        c = self._index_to_mask_index(c)
-        return self.get(c)
+    def get_mask(self, k):
+        k = self._index_to_mask_index(k)
+        return self.get(k)
 
-    def getbit_mask(self, c, i):
-        c = self._index_to_mask_index(c)
-        return self.getbit(c, i)
+    def getbit_mask(self, k, i):
+        k = self._index_to_mask_index(k)
+        return self.getbit(k, i)
