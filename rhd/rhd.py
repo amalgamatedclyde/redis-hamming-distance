c7bfd59f0478c6813d8fc3cf9036249c3f884a75
Phelim Bradley <wave@phel.im>
2016-09-10 15:44:12 +0100

    Automatically pipe arguments if given list

--- rhd/rhd.py
+++ rhd/rhd.py
@@ -1,19 +1,63 @@
 from __future__ import print_function
+from rhd.decorators import pipeiflist
 import crc16
 
 
-class Rhd(object):
+class RedisCluster(object):
 
     def __init__(self, connections):
         # list of redis connections
         self.connections = connections
         self.num_conns = len(self.connections)
 
-    def set(self, c, _bytes):
-        self.get_connection(c).set(c, _bytes)
+    @pipeiflist
+    def set(self, c, _bytes, conn=None):
+        conn.set(c, _bytes)
 
-    def setbit(self, c, i, bit):
-        self.get_connection(c).setbit(c, i, bit)
+    @pipeiflist
+    def setbit(self, c, i, bit, conn=None):
+        conn.setbit(c, i, bit)
+
+    @pipeiflist
+    def get(self, c, conn=None):
+        return conn.get(c)
+
+    @pipeiflist
+    def getbit(self, c, i, conn=None):
+        return conn.getbit(c, i)
+
+    def get_connection(self, c):
+        if isinstance(c, str):
+            c = str.encode(c)
+        elif isinstance(c, int):
+            c = str.encode(str(c))
+        return self._get_connection_from_crc16(crc16.crc16xmodem(c))
+
+    def get_connection_index(self, c):
+        if isinstance(c, str):
+            c = str.encode(c)
+        elif isinstance(c, int):
+            c = str.encode(str(c))
+        return self._get_connection_index_from_crc16(crc16.crc16xmodem(c))
+
+    def _get_connection_index_from_crc16(self, crc16):
+        return crc16 % self.num_conns
+
+    def _get_connection_from_crc16(self, crc16):
+        index = self._get_connection_index_from_crc16(crc16)
+        return self.connections[index]
+
+    def _create_pipelines(self):
+        return [c.pipeline() for c in self.connections]
+
+
+class Rhd(RedisCluster):
+
+    def __init(self, connections):
+        super().__init__(connections)
+
+    def _index_to_mask_index(self, c):
+        return "".join([str(c), "_mask"])
 
     def set_mask(self, c, _bytes):
         c = self._index_to_mask_index(c)
@@ -23,12 +67,6 @@ class Rhd(object):
         c = self._index_to_mask_index(c)
         self.setbit(c, i, bit)
 
-    def get(self, c):
-        return self.get_connection(c).get(c)
-
-    def getbit(self, c, i):
-        return self.get_connection(c).getbit(c, i)
-
     def get_mask(self, c):
         c = self._index_to_mask_index(c)
         return self.get(c)
@@ -36,17 +74,3 @@ class Rhd(object):
     def getbit_mask(self, c, i):
         c = self._index_to_mask_index(c)
         return self.getbit(c, i)
-
-    def _index_to_mask_index(self, c):
-        return "".join([str(c), "_mask"])
-
-    def get_connection(self, c):
-        if isinstance(c, str):
-            c = str.encode(c)
-        elif isinstance(c, int):
-            c = str.encode(str(c))
-        return self._get_connection_from_crc16(crc16.crc16xmodem(c))
-
-    def _get_connection_from_crc16(self, crc16):
-        index = crc16 % self.num_conns
-        return self.connections[index]
--- rhd/tests/test_rhd.py
+++ rhd/tests/test_rhd.py
@@ -11,51 +11,6 @@ conn2 = redis.StrictRedis(
 connections = [conn1, conn2]
 
 
-@given(integers())
-def test_choose_connection(i):
-    rhd = Rhd(connections)
-    assert rhd._get_connection_from_crc16(i) in connections
-
-
-@given(text(min_size=1))
-def test_choose_connection_str(i):
-    rhd = Rhd(connections)
-    assert rhd.get_connection(i) in connections
-
-
-@given(integers())
-def test_choose_connection_int(i):
-    rhd = Rhd(connections)
-    assert rhd.get_connection(i) == rhd.get_connection(str(i))
-
-
-@given(text())
-def test_choose_connection_bytes(i):
-    rhd = Rhd(connections)
-    assert rhd.get_connection(i) == rhd.get_connection(str.encode(i))
-
-
-@given(c=text(min_size=1), _bytes=binary(min_size=1))
-def test_set_get_text(c, _bytes):
-    rhd = Rhd(connections)
-    rhd.set(c, _bytes)
-    assert rhd.get(c) == _bytes
-
-
-@given(c=integers(), _bytes=binary(min_size=1))
-def test_set_get_int(c, _bytes):
-    rhd = Rhd(connections)
-    rhd.set(c, _bytes)
-    assert rhd.get(c) == _bytes
-
-
-@given(c=text(min_size=1), i=integers(min_value=0, max_value=100000), bit=integers(min_value=0, max_value=1))
-def test_setbit_getbit_int(c, i, bit):
-    rhd = Rhd(connections)
-    rhd.setbit(c, i, bit)
-    assert rhd.getbit(c, i) == bit
-
-
 @given(c=text(), _bytes=binary(min_size=1))
 def test_set_get_mask(c, _bytes):
     rhd = Rhd(connections)
